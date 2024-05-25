580428de3b4239be6b2d18fadee6dec7e0dd547b
liuhongli <>
2024-05-25 18:19:07 +0800

    [t0]Create MagicKit Project for network request and Parsing JSON data

---
+++ OCDemo/OCDemo/OCDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  OCDemoApp.swift
+//  OCDemo
+//
+//  Created by liuhongli on 2024/5/25.
+//
+
+import SwiftUI
+
+@main
+struct OCDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftDemo/SwiftDemo/SwiftDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDemoApp.swift
+//  SwiftDemo
+//
+//  Created by liuhongli on 2024/5/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
